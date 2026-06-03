2de9edcf6c7711366a313b4f68801f859c87359e
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2026-06-03 13:06:07 +0300

    feat: добавил классам приватные поля и геттеры

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import ListPresenter from './presenter/list-presenter.js';
-import SortView from './view/sort-view.js';
 import TripInfoView from './view/trip-info-view.js';
 import FilterView from './view/filter-view.js';
 import { render, RenderPosition } from './framework/render.js';
@@ -11,26 +10,20 @@ const header = document.querySelector('.page-header');
 const tripInfoContainer = header.querySelector('.trip-main');
 const filterContainer = header.querySelector('.trip-controls__filters');
 
-// нахожу контейнер для основного содержимого в main
-const main = document.querySelector('.page-main');
-const tripEventsContainer = main.querySelector('.trip-events');
-
 // cоздаю компоненты шапки - информацию о путешествии и блок фильтровки
 const tripInfoComponent = new TripInfoView();
 const filterComponent = new FilterView();
-
-// создаю компонент main - блок сортировки точек путешествия
-const sortComponent = new SortView();
-
-// получаю данные по точкам путешествия из модели точек
-const pointsModel = new PointsModel(offersData, destinationsData);
 
 // отрисовываю компоненты в header (информацию о путешествии и блок фильтровки)
 render (tripInfoComponent, tripInfoContainer, RenderPosition.AFTERBEGIN);
 render (filterComponent, filterContainer);
 
-// отрисовываю компонент сортировки в main
-render (sortComponent, tripEventsContainer);
+// нахожу контейнер для основного содержимого в main
+const main = document.querySelector('.page-main');
+const tripEventsContainer = main.querySelector('.trip-events');
+
+// получаю данные по точкам путешествия из модели точек, передав внутрь модели данные offers и destinations из мок.данных
+const pointsModel = new PointsModel(offersData, destinationsData);
 
 // передаю в презентер списка - контейнер основного содержимого и данные о точках путешествия полученные из модели точек
 const ListComponent = new ListPresenter({
--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -2,30 +2,38 @@ import ListTripEvents from '../view/list-trip-view.js';
 import PointTripEvent from '../view/point-trip-view.js';
 import FormEditEvent from '../view/form-edit-view.js';
 import { render } from '../framework/render.js';
+import SortView from '../view/sort-view.js';
 
 export default class ListPresenter {
-  listEventComponent = new ListTripEvents();
+  #listContainer = null;
+  #pointsModel = {};
+  #sortComponent = new SortView();
+  #listEventComponent = new ListTripEvents();
+  #listPoints = [];
+  #listOffers = [];
+  #listDestinations = [];
 
   constructor ({ container, pointsModel }) {
-    this.listContainer = container; // container - tripEventsContainer приходит из точки входа - контейнер для списка точек путешествия;
-    this.pointsModel = pointsModel; // массив точек путешествия;
+    this.#listContainer = container; // container - tripEventsContainer приходит из точки входа - контейнер для списка точек путешествия;
+    this.#pointsModel = pointsModel; // массив точек путешествия;
   }
 
   init () {
     // делаю копию массива точек, чтобы случайно не мутировать данные - это временное решение
-    this.listPoints = [...this.pointsModel.getPoints()];
-    this.listOffers = [...this.pointsModel.getOffers()];
-    this.listDestinations = [...this.pointsModel.getDestinations()];
+    this.#listPoints = [...this.#pointsModel.points];
+    this.#listOffers = [...this.#pointsModel.offers];
+    this.#listDestinations = [...this.#pointsModel.destinations];
 
+    render(this.#sortComponent, this.#listContainer);
     //отрисоваваю контейнер списка - <ul></ul>
-    render (this.listEventComponent, this.listContainer);
+    render (this.#listEventComponent, this.#listContainer);
 
     //отрисовываю форму редактирования точки списка - первым элементом списка
-    render (new FormEditEvent ({ point: this.listPoints[0], offers: this.listOffers, destinations: this.listDestinations }), this.listEventComponent.element);
+    render (new FormEditEvent ({ point: this.#listPoints[0], offers: this.#listOffers, destinations: this.#listDestinations }), this.#listEventComponent.element);
 
-    for (let i = 1; i < this.listPoints.length; i += 1) {
+    for (let i = 1; i < this.#listPoints.length; i += 1) {
       // отрисовываю точки списка по одной из массива точек, который приходит из модели
-      render(new PointTripEvent({ point: this.listPoints[i], offers: this.listOffers }), this.listEventComponent.element);
+      render(new PointTripEvent({ point: this.#listPoints[i], offers: this.#listOffers }), this.#listEventComponent.element);
     }
   }
 }
